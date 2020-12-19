1f4eb97d20fb0e4d0279f33e28be76c88dd8f91f
lumizila <lumiwille@gmail.com>
2020-12-19 11:39:22 +0900

    test

---
+++ controller.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+     $("#searchbutton").click(function(){
+            //$.get('https://kyoto-map-20.herokuapp.com/', function(data, status){
+             //$("#search").html(data);
+             //alert(status)  
+            //});
+            alert("hello!")
+     });
+});
