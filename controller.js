99ab887b9a7102290c467a4cc7a3f9f02d31c63c
lumizila <lumiwille@gmail.com>
2020-12-19 14:39:37 +0900

    fixing error

--- controller.js
+++ controller.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	initMap();
 	$("#searchRoute").click(function(){
 		$.get('https://kyoto-map-20.herokuapp.com/',function(data,status){
 			alert(data)
@@ -12,14 +11,3 @@ $(document).ready(function(){
 
 });
 
-function initMap(){
-			var location = {lat: 35.011635, lng: 135.768036};
-			var map = new google.maps.Map(document.getElementById("map"), {
-				zoom: 4,
-				center: location
-			});
-			var marker = new google.maps.Marker({
-				position: location,
-				map: map
-			});
-		}
