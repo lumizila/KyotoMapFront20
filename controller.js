bb17f17d2fb8be491fd78b2792e2ac760e494c25
Tianzejie <1017943372@qq.com>
2021-01-06 16:14:42 +0900

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,4 +1,7 @@
-  $("#tyaExample").click(function(){
+$(document).ready(function(){
+
+
+$("#tyaExample").click(function(){
     $.get('https://kyoto-map-20.herokuapp.com/locations/',function(data,status){
       alert("The jpname, description, latitude of the first location returned is:")
       alert("jpname: "+data[0].jpname+" description: "+data[0].description+" latitude: "+data[0].lat);
@@ -8,40 +11,69 @@
       alert("jpname: "+data[1].jpname+" description: "+data[1].description+" latitude: "+data[1].lat);
     });
   });
-  
+
+$("#nature").click(function(){
+  $.get('https://kyoto-map-20.herokuapp.com/locations/',function(data,status){
+    alert("The jpname, description, latitude of the first location returned is:")
+    alert("jpname: "+data[0].jpname+" description: "+data[0].description+" latitude: "+data[0].lat);
+    alert("Only the category of the first location locations is...")
+    alert(data[0].category);
+    alert("The jpname, description, latitude of the second location returned is:")
+    alert("jpname: "+data[1].jpname+" description: "+data[1].description+" latitude: "+data[1].lat);
+  });
+});
+
+
+});
+
+
+
+
+
 function initMap() {
     const garden = { lat: 35.023138536118545, lng: 135.76386160876868 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 14,
       center: garden,
     });
-    const contentString =
-      '<div id="content">' +
-      '<div id="siteNotice">' +
-      "</div>" +
-      '<h1 id="firstHeading" class="firstHeading">Kyoto Botanical Garden</h1>' +
-      '<div id="bodyContent">' +
-      "<p><b>Kyoto Botanical Garden</b>, Founded in 1924, Kyoto Botanical Gardens is the oldest and most comprehensive public botanical garden in Japan.</b> " +
-      "With seasonal plants and various conservatories, the Gardens is ever-changing and can be enjoyed all year round</b> " +
-      "More than 450 cherry blossom trees bloom beautifully in spring and leaves of deciduous trees such as maple color exquisitely in autumn. " +
-      "At other times of the year, lush greenery and a wide variety of trees and flowers can be enjoyed" +
-      "Kyoto Botanical Gardens is a living museum of plants that offers opportunities for both education and relaxation. " +
-      "Total area: approximately 240,000 ㎡ " +
-      "Collection: approximately 12,000 taxa " +
-      '<p>Attribution: Kyoto Botanical Garden, <a href="http://www.pref.kyoto.jp/en/02-02-10.html">' +
-      "http://www.pref.kyoto.jp/en/02-02-10.html</a> " +
-      "</div>" +
-      "</div>";
-    const infowindow = new google.maps.InfoWindow({
-      content: contentString,
-    });
-    const marker = new google.maps.Marker({
-      position: garden,
-      map,
-      title: "Kyoto Botanical Garden",
-    });
-    marker.addListener("click", () => {
-      infowindow.open(map, marker);
-    });
-  }
 
+
+    $.get('https://kyoto-map-20.herokuapp.com/locations/',function(data,status){
+
+      var locations=new Array();
+      for (var i=0;i<data.length;i++){
+        var t_lat=parseFloat(data[i].lon);
+        var t_lng=parseFloat(data[i].lat);
+        locations.push({lat:t_lat, lng:t_lng, description:data[i].description, name:data[i].jpname});
+      }
+      const image ="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+      for (var i=0;i<locations.length;i++){
+        const location=locations[i];
+
+        marker=new google.maps.Marker({
+
+          position:{lat:location.lat,lng:location.lng},
+          map,
+          icon:image,
+        });
+
+        const contentString =
+          '<div id="content">' +
+          '<div id="siteNotice">' +
+          "</div>" +
+          '<h1 id="firstHeading" class="firstHeading">'+location.name+'</h1>' +
+          '<div id="bodyContent">' +
+          "<p>"+ location.description+"</p>"
+          "</div>" +
+          "</div>";
+        const infowindow = new google.maps.InfoWindow({
+          content: contentString,
+        });
+
+        marker.addListener("click", () => {
+          infowindow.open(map, marker);
+        });
+      };
+      });
+  }
